cd70ade9c6462b47245fe46193e677ffbf9b8423
Narasimhareddy-dev <posttonarsi30@gmail.com>
2026-02-17 11:13:22 +0530

    Factorial of a number.

---
+++ Fact.java
@@ -0,0 +1,13 @@
+public class Fact {
+    public static void main(String[] args) {
+
+        int num = 5;   // stored number
+        int fact = 1;
+
+        for(int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial of " + num + " is: " + fact);
+    }
+}
